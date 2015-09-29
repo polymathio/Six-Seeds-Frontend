3ff80ab7f3c77300429f93c5f2cfc50fcdd22dc2
Phillip Jon Ambas <ambasp@gmail.com>
2015-09-29 10:49:17 -0400

    add ads.js module

--- dist/public/scripts/scripts.js
+++ dist/public/scripts/scripts.js
@@ -1,13 +1,19 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var modal = require('./modules/modal.js');
 var comments = require('./modules/comments.js');
+var ads = require('./modules/ads.js');
 
 $(document).ready(function(){
 	modal.init();
 	comments.init();
+	ads();
 });
 
-},{"./modules/comments.js":2,"./modules/modal.js":3}],2:[function(require,module,exports){
+},{"./modules/ads.js":2,"./modules/comments.js":3,"./modules/modal.js":4}],2:[function(require,module,exports){
+module.exports = function(){
+	alert('test ad module');
+};
+},{}],3:[function(require,module,exports){
 var comments = {
 	init: function() {
 		this.cacheDom();
@@ -33,7 +39,7 @@ var comments = {
 }
 
 module.exports = comments;
-},{}],3:[function(require,module,exports){
+},{}],4:[function(require,module,exports){
 var modal = {
 	init: function() {
 		this.cacheDom();
--- src/public/scripts/scripts.js
+++ src/public/scripts/scripts.js
@@ -1,7 +1,9 @@
 var modal = require('./modules/modal.js');
 var comments = require('./modules/comments.js');
+var ads = require('./modules/ads.js');
 
 $(document).ready(function(){
 	modal.init();
 	comments.init();
+	ads();
 });
